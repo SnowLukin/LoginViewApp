8c472a453f4e60d41c72369aaad7c50efa9c5e41
Snow Lukin <everyoursden@gmail.com>
2021-12-15 00:09:32 +0300

    Added main functionalities

---
+++ LoginView/WelcomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  WelcomeViewController.swift
+//  LoginView
+//
+//  Created by Snow Lukin on 14.12.2021.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
+    var userName: String!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        welcomeLabel.text = "Welcome, \(userName ?? "")!"
+    }
+    
+    @IBAction func logoutClicked() {
+        dismiss(animated: true)
+    }
+    
+}
