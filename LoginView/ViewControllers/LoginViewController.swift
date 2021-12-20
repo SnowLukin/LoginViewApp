cf8d35a2f8ed6b1ca8fcf278bde623e21225cfce
Snow Lukin <everyoursden@gmail.com>
2021-12-20 16:48:03 +0300

    Cosmetic fixes

--- LoginView/ViewControllers/LoginViewController.swift
+++ LoginView/ViewControllers/LoginViewController.swift
@@ -14,6 +14,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var userNameTF: UITextField!
     @IBOutlet weak var passwordTF: UITextField!
     
+    let users = [User.getUser()]
     
     // MARK: - Life cycle
     
